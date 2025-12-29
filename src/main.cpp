14605bd3b1f680ccd2417b7b51b35146bd8e7316
KotaNofa <kokaboba@protonmail.com>
2025-12-28 20:52:25 -0800

    Added texture class and some textures

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,32 @@
 #include "utils.h"   // framebuffer resize callback and other utilities
 
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "stb/stb_img.h"
 
+#include <GLFW/glfw3.h>
 #include "opengl/shader.h"
 #include "opengl/vao.h"
 #include "opengl/vbo.h"
 #include "opengl/ebo.h"
+#include "opengl/texture.h"
 
 #include <iostream>
 #include <math.h>
 
 // Vertices coordinates
 GLfloat vertices[] =
-{ //               COORDINATES                  /     COLORS           //
-	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
 };
 
-// Index buffer defining three triangles using the vertices above
-GLuint indices[] = 
+// Indices for vertices order
+GLuint indices[] =
 {
-    0, 3, 5,
-    3, 2, 4,
-    5, 4, 1
+	0, 2, 1, // Upper triangle
+	0, 3, 2 // Lower triangle
 };
 
 int main()
@@ -80,8 +79,9 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Associate the VBO with the VAO and define vertex attribute layout
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
 	// Unbind objects to avoid accidental state modification
 	VAO1.Unbind();
@@ -89,6 +89,9 @@ int main()
 	EBO1.Unbind();
 
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+    Texture boub("assets/textures/boub.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    boub.texUnit(shaderProgram, "tex0", 0);
 
     // Main render loop
 	while (!glfwWindowShouldClose(window))
@@ -99,12 +102,15 @@ int main()
 
 		// Activate the shader program
 		shaderProgram.Activate();
-        glUniform1f(uniID, 0.5f);
-		// Bind the VAO containing vertex and index state
-		VAO1.Bind();
+        
+        // Bind the VAO containing vertex and index state
+		glUniform1f(uniID, 0.5f);
+        boub.Bind();
+        
+        VAO1.Bind();
 
 		// Draw indexed triangles using the element buffer
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Swap the front and back buffers
 		glfwSwapBuffers(window);
