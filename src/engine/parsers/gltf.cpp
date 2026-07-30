cb660d41035a88c77455f1636b5409173980bfdd
kokalikesboba <kokaboba@protonmail.com>
2026-07-30 14:55:03 -0700

    DNC: gltf parser adjustments

--- src/engine/parsers/gltf.cpp
+++ src/engine/parsers/gltf.cpp
@@ -2,136 +2,35 @@
 
 using namespace RenderFormat;
 
-ParseGLTF::ParseGLTF(const std::string& modelDir) {
+// I'm struggling to read the syntax a little bit, so member variables using this-> is intentional
 
-    // Placeholder on file path fail
-    std::filesystem::path path = modelDir;
-    if (!std::filesystem::exists(path)) {
-        std::cerr << "[ERROR][Parser] Invalid model path: " << modelDir << std::endl;
-        LoadShameModel();
-        return;
-    }
+ParseGLTF::ParseGLTF(const std::string& modelDir) :
+    modelDir(modelDir)
+{
+    this->path = std::filesystem::path(modelDir);
+    if (!FilePathCheck()) return;
 
-    // Placeholder if fastgltf error returns an error
-    auto dataBuffer = fastgltf::GltfDataBuffer::FromPath(modelDir);
-    if (dataBuffer.error() != fastgltf::Error::None) {
-        std::cerr << "[ERROR][Parser] Could not read file: " << modelDir << std::endl;
-        LoadShameModel();
-        return;
-    }
+    auto databuffer = fastgltf::GltfDataBuffer::FromPath(path);
+    if (!DataBufferCheck(databuffer)) return;
 
-    // Actual parsing starts here.
-    fastgltf::Parser parser;
-    auto loadedAsset = parser.loadGltf(dataBuffer.get(), path.parent_path(), fastgltf::Options::LoadExternalBuffers);
-    // Placeholder if parser returns error
-    if (loadedAsset.error() != fastgltf::Error::None) {
-        std::cerr << "[ERROR][Parser] Failed to parse glTF: " << modelDir << " - " << fastgltf::getErrorMessage(loadedAsset.error()) << std::endl;
-        LoadShameModel();
-        return;
-    }
+    auto loadedAsset = this->parser.loadGltf(databuffer.get(), path.parent_path(), fastgltf::Options::LoadExternalBuffers);
+    if (!AssetCheck(loadedAsset)) return;
+    asset = std::move(loadedAsset.get());
+    partCount = asset.meshes.size();
 
-    fastgltf::Asset& asset = loadedAsset.get();
-    meshCount= asset.meshes.size();
-    vertices.reserve(meshCount);
-    indices.reserve(meshCount);
-    texData.reserve(meshCount);
-
+    ModelRenderData entry{};
     for (auto& mesh : asset.meshes) {
+        for (auto& buffer : mesh.primitives) {
+            entry.vertices.emplace_back(std::move(ParseVertices(buffer)));
+            entry.indices.emplace_back(std::move(ParseIndices(buffer)));
 
-        for (auto& prim : mesh.primitives) {
-
-            size_t baseVertex = vertices.size();
-
-            fastgltf::Attribute* posAttr = prim.findAttribute("POSITION");
-            fastgltf::Accessor& posAccessor = asset.accessors[posAttr->accessorIndex];
-
-            std::vector<RenderFormat::PNCUVertex> vertexEntry;
-            vertexEntry.resize(baseVertex + posAccessor.count);
-
-            for (size_t i = 0; i < posAccessor.count; ++i) {
-                auto pos = fastgltf::getAccessorElement<fastgltf::math::fvec3>(asset, posAccessor, i);
-                vertexEntry[baseVertex + i].position = {pos.x(), pos.y(), pos.z()};
-            }
-
-            fastgltf::Attribute* normalAttribute = prim.findAttribute("NORMAL");
-            fastgltf::Accessor& normalAccessor = asset.accessors[normalAttribute->accessorIndex];
-            for (size_t i = 0; i < normalAccessor.count; ++i) {
-                auto normal = fastgltf::getAccessorElement<fastgltf::math::fvec3>(asset, normalAccessor, i);
-                vertexEntry[baseVertex + i].normal = {normal.x(), normal.y(), normal.z()};
-            } 
-
-            fastgltf::Attribute* uvAttribute = prim.findAttribute("TEXCOORD_0");
-            fastgltf::Accessor& uvAccessor = asset.accessors[uvAttribute->accessorIndex];
-            for (size_t i = 0; i < posAccessor.count; ++i) {
-                auto uv = fastgltf::getAccessorElement<fastgltf::math::fvec2>(asset, uvAccessor, i);
-                vertexEntry[baseVertex + i].uv = {uv.x(), uv.y()};    
-            }
-
-            vertices.emplace_back(std::move(vertexEntry));
-
-            std::vector<unsigned int> indexEntry;
-            if (prim.indicesAccessor.has_value()) {
-                fastgltf::Accessor& idxAccessor = asset.accessors[prim.indicesAccessor.value()];
-                indexEntry.reserve(indices.size() + idxAccessor.count);
-
-                for (size_t i = 0; i < idxAccessor.count; ++i) {
-                    auto index = fastgltf::getAccessorElement<unsigned int>(asset, idxAccessor, i);
-                    indexEntry.push_back(baseVertex + index);
-                }
-            }
-
-            indices.emplace_back(std::move(indexEntry));
         }
     }
 
+    RenderFormat::TextureData TextureData;
     for (auto& materials : asset.materials) {
-
         if (materials.pbrData.baseColorTexture.has_value()) {
-            auto& texture = asset.textures[materials.pbrData.baseColorTexture->textureIndex];
-            auto& image = asset.images[texture.imageIndex.value()];
-
-            // Absolute fucking magic to me
-            auto* bufferViewSource = std::get_if<fastgltf::sources::BufferView>(&image.data);
-            if (!bufferViewSource) {
-                LoadShameTexture(0,0);
-                continue;
-            }
-
-            auto& bufferView = asset.bufferViews[bufferViewSource->bufferViewIndex];
-            auto& buffer     = asset.buffers[bufferView.bufferIndex];
-
-            auto* bufferArray = std::get_if<fastgltf::sources::Array>(&buffer.data);
-            if (!bufferArray) {
-                LoadShameTexture(0,0);
-                continue;
-            }
-
-            const unsigned char* base = reinterpret_cast<const unsigned char*>(bufferArray->bytes.data());
-            const unsigned char* pngData = base + bufferView.byteOffset;
-            size_t pngLength = bufferView.byteLength;
-
-            ParsePNG png(pngData, pngLength);
-            if (png.data == nullptr) {
-                LoadShameTexture(0,0);
-                continue;
-            }
-            std::vector<unsigned char> bytes(png.data, png.data + png.width * png.height * 4);
-
-            std::vector<RenderFormat::TextureData> texEntry;
             
-            texEntry.push_back(
-                {
-                    png.width,
-                    png.height,
-                    png.hash,
-                    TexType::BaseColor,
-                    bytes
-                }
-            );
-            
-            texData.emplace_back(std::move(texEntry));
-        } else {
-            LoadShameTexture(0,0);
         }
     }
 }
@@ -164,4 +63,124 @@ void ParseGLTF::LoadShameTexture(int meshIndex, int texIndex)
         TexType::BaseColor,
         std::vector<unsigned char>{}
     };
+}
+
+bool ParseGLTF::FilePathCheck()
+{
+    if (!std::filesystem::exists(path)) {
+        std::cerr << "[ERROR][Parser] Invalid model path: " << modelDir << std::endl;
+        LoadShameModel();
+        return false;
+    }
+    return true;
+}
+
+bool ParseGLTF::DataBufferCheck(const fastgltf::Expected<fastgltf::GltfDataBuffer>& databuffer)
+{
+    if (databuffer.error() != fastgltf::Error::None) {
+        std::cerr << "[ERROR][Parser] Could not read file: " << modelDir << std::endl;
+        LoadShameModel();
+        return false;
+    }
+    return true;
+}
+
+bool ParseGLTF::AssetCheck(const fastgltf::Expected<fastgltf::Asset>& loadedAsset)
+{
+    if (loadedAsset.error() != fastgltf::Error::None) {
+        std::cerr << "[ERROR][Parser] Failed to parse glTF: " << modelDir << " - " << fastgltf::getErrorMessage(loadedAsset.error()) << std::endl;
+        LoadShameModel();
+        return false;
+    }
+    return true;
+}
+
+std::vector<RenderFormat::PNCUVertex> ParseGLTF::ParseVertices(fastgltf::Primitive &buffer)
+{
+    fastgltf::Attribute* posAttr = buffer.findAttribute("POSITION");
+    fastgltf::Accessor& posAccessor = asset.accessors[posAttr->accessorIndex];
+
+    std::vector<RenderFormat::PNCUVertex> vertexEntry;
+    vertexEntry.resize(posAccessor.count);
+
+    for (size_t i = 0; i < posAccessor.count; ++i) {
+        auto pos = fastgltf::getAccessorElement<fastgltf::math::fvec3>(asset, posAccessor, i);
+        vertexEntry[i].position = {pos.x(), pos.y(), pos.z()};
+    }
+
+    fastgltf::Attribute* normalAttribute = buffer.findAttribute("NORMAL");
+    fastgltf::Accessor& normalAccessor = asset.accessors[normalAttribute->accessorIndex];
+    for (size_t i = 0; i < normalAccessor.count; ++i) {
+        auto normal = fastgltf::getAccessorElement<fastgltf::math::fvec3>(asset, normalAccessor, i);
+        vertexEntry[i].normal = {normal.x(), normal.y(), normal.z()};
+    } 
+
+    fastgltf::Attribute* uvAttribute = buffer.findAttribute("TEXCOORD_0");
+    fastgltf::Accessor& uvAccessor = asset.accessors[uvAttribute->accessorIndex];
+    for (size_t i = 0; i < posAccessor.count; ++i) {
+        auto uv = fastgltf::getAccessorElement<fastgltf::math::fvec2>(asset, uvAccessor, i);
+        vertexEntry[i].uv = {uv.x(), uv.y()};    
+    }
+    return vertexEntry;
+}
+
+std::vector<unsigned int> ParseGLTF::ParseIndices(fastgltf::Primitive &buffer)
+{
+    std::vector<unsigned int> indexEntry;
+    if (buffer.indicesAccessor.has_value()) {
+        fastgltf::Accessor& idxAccessor = asset.accessors[buffer.indicesAccessor.value()];
+
+        for (size_t i = 0; i < idxAccessor.count; ++i) {
+            auto index = fastgltf::getAccessorElement<unsigned int>(asset, idxAccessor, i);
+            indexEntry.push_back(index);
+        }
+    }
+}
+
+RenderFormat::TextureData ParseGLTF::ParseTexture(fastgltf::Material &material)
+{
+    auto& texture = asset.textures[material.pbrData.baseColorTexture->textureIndex];
+        auto& image = asset.images[texture.imageIndex.value()];
+
+        auto* bufferViewSource = std::get_if<fastgltf::sources::BufferView>(&image.data);
+        if (!bufferViewSource) {
+            LoadShameTexture(0,0);
+            continue;
+        }
+
+        auto& bufferView = asset.bufferViews[bufferViewSource->bufferViewIndex];
+        auto& buffer     = asset.buffers[bufferView.bufferIndex];
+
+        auto* bufferArray = std::get_if<fastgltf::sources::Array>(&buffer.data);
+        if (!bufferArray) {
+            LoadShameTexture(0,0);
+            continue;
+        }
+
+        const unsigned char* base = reinterpret_cast<const unsigned char*>(bufferArray->bytes.data());
+        const unsigned char* pngData = base + bufferView.byteOffset;
+        size_t pngLength = bufferView.byteLength;
+
+        ParsePNG png(pngData, pngLength);
+        if (png.data == nullptr) {
+            LoadShameTexture(0,0);
+            continue;
+        }
+        std::vector<unsigned char> bytes(png.data, png.data + png.width * png.height * 4);
+
+        std::vector<RenderFormat::TextureData> texEntry;
+        
+        texEntry.push_back(
+            {
+                png.width,
+                png.height,
+                png.hash,
+                TexType::BaseColor,
+                bytes
+            }
+        );
+        
+        texData.emplace_back(std::move(texEntry));
+    } else {
+    LoadShameTexture(0,0);
 }
--- src/engine/parsers/gltf.h
+++ src/engine/parsers/gltf.h
@@ -14,16 +14,45 @@
 #include <filesystem>
 #include <memory>
 
+
+    struct PNCUVertex {
+        glm::vec3 position;
+        glm::vec3 normal;
+        glm::vec3 color;
+        glm::vec2 uv;
+    };
+    
+struct ModelRenderData {
+    std::vector<RenderFormat::PNCUVertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<RenderFormat::TextureData> texData;
+    glm::vec3 position;
+    glm::quat orientation;
+    glm::vec3 scale;
+};
+
 class ParseGLTF {
 public:
     ParseGLTF(const std::string& modelDir);
-    std::vector<std::vector<RenderFormat::PNCUVertex>> vertices;
-    std::vector<std::vector<unsigned int>> indices;
-    std::vector<std::vector<RenderFormat::TextureData>> texData;
-    int meshCount;
-private:
+    std::vector<ModelRenderData> data;
+    int partCount;
+protected:
     void LoadShameModel();
     void LoadShameTexture(int meshIndex, int texIndex);
+
+    bool FilePathCheck();
+    bool DataBufferCheck(const fastgltf::Expected<fastgltf::GltfDataBuffer>& databuffer);
+    bool AssetCheck(const fastgltf::Expected<fastgltf::Asset>& loadedAsset);
+
+    std::vector<RenderFormat::PNCUVertex> ParseVertices(fastgltf::Primitive &buffer);
+    std::vector<unsigned int> ParseIndices(fastgltf::Primitive &buffer);
+    
+    RenderFormat::TextureData ParseTexture(fastgltf::Material &material);
+private:
+    std::string modelDir;
+    std::filesystem::path path;
+    fastgltf::Parser parser;
+    fastgltf::Asset asset;
 };  
 
 #endif
